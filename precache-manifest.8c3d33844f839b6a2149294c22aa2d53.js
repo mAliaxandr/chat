b886244827fe2c827051b2ec470aafffe0e6a9a9
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2019-08-11 14:49:35 +0300

    init: start chat

---
+++ precache-manifest.8c3d33844f839b6a2149294c22aa2d53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fed6d49e92ea82454f77fc0841c06e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3894ed6df3dc4ed5b7b9",
+    "url": "/static/css/main.f11b0f8e.chunk.css"
+  },
+  {
+    "revision": "6fecf016f9dfdb62142a",
+    "url": "/static/js/2.1e36eaba.chunk.js"
+  },
+  {
+    "revision": "3894ed6df3dc4ed5b7b9",
+    "url": "/static/js/main.9ffa7944.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
